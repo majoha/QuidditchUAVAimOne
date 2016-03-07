1f98a4bc272f5fb64a96dd87a51e6feb91228a8d
majoha <oufcmh@yahoo.co.uk>
2016-03-07 12:42:19 +0000

    Another test commit

--- Properties/Resource.Designer.cs
+++ Properties/Resource.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Tutorial_31___AR_Drone {
+namespace Tutorial_31___AR_Drone.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Tutorial_31___AR_Drone {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Tutorial_31___AR_Drone.Resource", typeof(Resource).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Tutorial_31___AR_Drone.Properties.Resource", typeof(Resource).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
